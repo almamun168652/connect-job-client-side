98886db95a538b1996c1e66eabdcc0e186223fc1
Al Mamun <wdmamun75@gmail.com>
2023-11-06 17:31:24 +0600

    added update applicant number

--- src/Pages/CardDetails/CardDetails.jsx
+++ src/Pages/CardDetails/CardDetails.jsx
@@ -3,15 +3,20 @@ import { useParams } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import swal from "sweetalert";
 
 const CardDetails = () => {
 
-    const { user } = useContext(AuthContext);
+    const today = new Date();
+    const currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const [currentYear, currentMonth, currentDay] = currentDate.split('-').map(Number);
 
+
+    const { user } = useContext(AuthContext);
     const { id } = useParams();
 
-    const { data, isLoading } = useQuery({
+    const { data, isLoading , isFetching } = useQuery({
         queryKey: ['jobsDetails'],
         queryFn: async () => {
             const data = await fetch(`http://localhost:5000/jobs/details/${id}`);
@@ -26,15 +31,117 @@ const CardDetails = () => {
     }
 
     const {
+        _id,
         banner,
         title,
         logo,
+        username,
+        jobCategory,
         salleryStart,
         salleryEnd,
         description,
-        applicantNumber
+        postingDate,
+        deadline,
+        applicantNumber,
+        email,
     } = data || {}
 
+
+
+    const applyDeadline = deadline.slice(0, 10);
+    const [deadlineYear, deadlineMonth, deadlineDay] = applyDeadline.split('-').map(Number);
+
+
+
+
+
+    const hanldemodalsubmit = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const modalUsername = form.modalUsername.value;
+        const modalEmail = form.modalEmail.value;
+        const modalResume = form.modalResume.value;
+
+
+
+        const appliedInfo = {
+            appliedUsername: modalUsername,
+            appliedEmail: modalEmail,
+            appliedResume: modalResume,
+            postBanner: banner,
+            postTitle: title,
+            companyLogo: logo,
+            postUsername: username,
+            jobCategory,
+            salleryStart,
+            salleryEnd,
+            postDescription: description,
+            postingDate,
+            deadline,
+            applicantNumber,
+            postEmail: email
+        }
+
+        // validation
+
+        // if (email === modalEmail) {
+        //     swal('Sorry', "You Can't apply your own job.", "warning");
+        //     return;
+        // }
+
+        // (year1 < year2 || (year1 === year2 && (month1 < month2 || (month1 === month2 && day1 < day2))))
+
+        if (currentYear < deadlineYear || (currentYear === deadlineYear) && (currentMonth < deadlineMonth || (currentMonth === deadlineMonth && currentDay < deadlineDay))) {
+            try {
+                const response = await fetch("http://localhost:5000/applied", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(appliedInfo),
+                });
+                const result = await response.json();
+
+
+                if (result.acknowledged) {
+                    swal('done', "Job Applied Successfully", "success");
+
+                    // updated number of applicant
+
+                    
+
+                    try{
+                        fetch(`http://localhost:5000/jobs/${_id}`, {
+                        method: "PATCH",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        // body: JSON.stringify({applicantNumber}),
+                    })
+                        .then((res) => res.json())
+                        .then((data) => {
+                            console.log(data);
+                        });
+                    }catch(error){
+                        console.log(error);
+                    }
+                    
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+
+            form.reset('');
+            return;
+        } else {
+            swal('Sorry', "Deadline has been expired.", "error");
+            return;
+        }
+
+
+
+    }
 
 
     return (
@@ -80,33 +187,44 @@ const CardDetails = () => {
 
                         <div className="mt-2">
                             <div className="flex justify-end items-center mt-2">
-                                <button onClick={() => document.getElementById('submitResume').showModal()} className='bg-[#152475] px-6 py-2 mt-1 rounded-md cursor-pointer text-white'>Apply Now</button>
+                                <label htmlFor="applyJobModal" className='bg-[#152475] px-6 py-2 mt-1 rounded-md cursor-pointer text-white'>Apply Now</label>
 
                                 {/* modal start */}
 
-                                <dialog id="submitResume" className="modal modal-bottom sm:modal-middle">
+                                {/* The button to open modal */}
+
+
+                                {/* Put this part before </body> tag */}
+                                <input type="checkbox" id="applyJobModal" className="modal-toggle" />
+                                <div className="modal">
                                     <div className="modal-box">
+                                        <label htmlFor="applyJobModal" className="flex justify-end">
+                                            <AiOutlineCloseCircle className="text-4xl"></AiOutlineCloseCircle>
+                                        </label>
+
                                         <h3 className="font-bold text-xl text-center">Apply To The Job</h3>
-                                        <div className="my-2">
-                                            <label className="text-gray-700">Your Username</label><br />
-                                            <input className="p-[6px] text-black rounded w-full border border-gray-500" defaultValue={user.displayName} type="text" name="username" id="" />
-                                        </div>
-                                        <div className="my-2">
-                                            <label className="text-gray-700">Your Email</label><br />
-                                            <input className="p-[6px] text-black rounded w-full border border-gray-500" defaultValue={user.email} type="text" name="username" id="" />
-                                        </div>
-                                        <div className="my-2">
-                                            <label className="text-gray-700">Your Resume Link</label><br />
-                                            <input placeholder="Your resume link" className="p-[6px] text-black rounded w-full border border-gray-500" type="text" name="username" id="" autoFocus/>
-                                        </div>
-                                        <div className="modal-action">
-                                            <form method="dialog">
-                                                {/* if there is a button in form, it will close the modal */}
-                                                <button className='bg-[#152475] px-4 py-1 mt-1 rounded-md cursor-pointer text-white'>Submit</button>
-                                            </form>
-                                        </div>
+                                        <form onSubmit={hanldemodalsubmit}>
+                                            <div className="my-2">
+                                                <label className="text-gray-700">Your Username</label><br />
+                                                <input className="p-[6px] text-black rounded w-full border border-gray-500" defaultValue={user.displayName} type="text" name="modalUsername" id="" />
+                                            </div>
+                                            <div className="my-2">
+                                                <label className="text-gray-700">Your Email</label><br />
+                                                <input className="p-[6px] text-black rounded w-full border border-gray-500" defaultValue={user.email} type="text" name="modalEmail" id="" />
+                                            </div>
+                                            <div className="my-2">
+                                                <label className="text-gray-700">Your Resume Link</label><br />
+                                                <input placeholder="Your resume link" className="p-[6px] text-black rounded w-full border border-gray-500" type="text" name="modalResume" id="" autoFocus />
+                                            </div>
+                                            <div method="dialog" className="flex justify-end">
+                                                <button type="submit" className="mt-4">
+                                                    <label className="bg-[#152475] px-8 py-2 rounded-md cursor-pointer text-white" htmlFor="applyJobModal">Submit</label>
+                                                </button>
+                                            </div>
+                                        </form>
+
                                     </div>
-                                </dialog>
+                                </div>
 
 
                                 {/* modal end */}
--- src/Pages/MyJobs/MyJobs.jsx
+++ src/Pages/MyJobs/MyJobs.jsx
@@ -1,6 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 
 const MyJobs = () => {
+
+    const { user , loading } = useContext(AuthContext)
+
+    const { data=[], isLoading } = useQuery({
+
+        queryKey:  ['abcd'],
+        
+        queryFn: async () => {
+            if (user?.email) {
+                const data = await fetch(`http://localhost:5000/jobs/${user?.email}`)
+                return await data.json();
+            }
+        } ,
+        enabled: !loading && !!user?.email
+    })
+
+    console.log(data);
+
+    // useEffect(()=>{
+    //     fetch(`http://localhost:5000/jobs/${user?.email}`)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data));
+    // } , [user?.email])
+
+
     return (
         <div>
             My Jobs
